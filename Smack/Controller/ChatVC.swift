174e5feef5b0daee5ea748d17ff9a66e16c8ff92
jainijhawan <jainijhawan@gmail.com>
2019-03-27 18:06:13 +0530

    End of Lesson 2

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Jai Nijhawan on 27/03/19.
+//  Copyright © 2019 Jai Nijhawan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    
+    }
+    
+
+
+
+}
